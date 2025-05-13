e9b1feb69aa7f0b8fea019ae39bbc72b53684bd7
balde <abdourahamane.22.balde@gmail.com>
2025-05-13 09:09:30 +0200

    Mise à jour de la configuration Angular, Cypress et fichiers dépendances

---
+++ Testez-une-application-full-stack/front/cypress/e2e/all.cy.ts
@@ -0,0 +1,9 @@
+// Importation des fichiers de tests E2E
+import './register-connect.cy';
+import './login.cy';
+import './create-session.cy';
+
+import './delete-session.cy';
+import './create-session.cy';
+import './edit-session.cy';
+import './suscribe-session.cy';
